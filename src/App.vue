b02e422ac8c23b48f951da2ae5bcc9235f4f39a7
Harry <harry750110@gmail.com>
2023-03-21 12:29:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+   
+
+
+   <BMI />
+  </div>
+</template>
+
+<script>
+
+import BMI from './components/BMI.vue';
+
+export default {
+  name: 'App',
+  components: {
+
+BMI
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+</style>
--- src/components/BMI.vue
+++ src/components/BMI.vue
@@ -1,89 +1,99 @@
 <template>
-  <div class="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
-    <div class="w-96 bg-white shadow-lg rounded-lg p-6 card">
-      <h1 class="text-2xl font-semibold mb-6">BMI 指數計算器</h1>
-      <div class="mb-4">
-        <label for="height" class="block text-sm mb-1">身高 (公分)</label>
-        <input type="number" v-model="height" id="height" placeholder="輸入您的身高"
-          class="w-full p-2 border border-gray-300 rounded">
+  <div class="container h-screen flex items-center justify-center">
+    <div class="inner rounded-lg bg-teal-500 p-6 relative" style="width: 350px;">
+     
+     <div class="B-wrap">
+      <span class="text-white font-bold absolute right-0 top-0 mr-4 mt-2">B</span>
+     <div class="content-padding"></div> 
+    </div>
+      <div id="BMI">
+        <form @submit.prevent="calculateBMI" class="mt-4">
+          <div class="form-group">
+            <label for="height" class="block text-white font-medium mb-2">身高 (cm):</label>
+            <input type="text" id="height" v-model="height" required class="form-input w-full">
+          </div>
+          <div class="form-group">
+            <label for="weight" class="block text-white font-medium mb-2">體重 (kg):</label>
+            <input type="text" id="weight" v-model="weight" required class="form-input w-full">
+          </div>
+          <div class="form-group mt-1">
+            <button type="submit"
+              class="bg-teal-600 text-white font-medium py-1 px-4 rounded-full hover:bg-teal-700 border border-white">計算</button>
+          </div>
+        </form>
+        <p v-if="bmi" class="mt-4 text-white font-medium">您的BMI是: {{ bmi }}</p>
       </div>
-      <div class="mb-6">
-        <label for="weight" class="block text-sm mb-1">體重 (公斤)</label>
-        <input type="number" v-model="weight" id="weight" placeholder="輸入您的體重"
-          class="w-full p-2 border border-gray-300 rounded">
-      </div>
-      <button @click="calculateBMI" class="w-full bg-blue-500 text-white py-2 rounded">計算 BMI</button>
-      <div v-if="bmi" class="mt-6 text-center">
-        <p class="text-lg">您的 BMI 指數為: <span class="font-semibold">{{ bmi.toFixed(1) }}</span></p>
-        <p class="text-lg">分類: <span class="font-semibold">{{ bmiCategory }}</span></p>
-      </div>
-      <transition name="fade">
-        <div v-if="showAnimation" class="fixed top-0 left-0 w-full h-full bg-blue-500 opacity-50"></div>
-      </transition>
     </div>
   </div>
 </template>
-
 <script>
   export default {
     data() {
       return {
-        height: "",
-        weight: "",
-        bmi: null,
-        showAnimation: false
-      };
-    },
-    computed: {
-      // eslint-disable-next-line vue/return-in-computed-property
-      bmiCategory() {
-        if (this.bmi < 18.5) return "過輕";
-        if (this.bmi >= 18.5 && this.bmi < 24.9) return "正常";
-        if (this.bmi >= 24.9 && this.bmi < 29.9) return "過重";
-        if (this.bmi >= 29.9) return "肥胖";
+        weight: '',
+        height: '',
+        bmi: ''
       }
     },
     methods: {
       calculateBMI() {
-        if (this.height && this.weight) {
-          this.bmi = (this.weight / ((this.height / 100) * (this.height / 100)));
-          this.showAnimation = true;
-          setTimeout(() => {
-            this.showAnimation = false;
-          }, 1000);
-        }
+        let bmi = this.weight / (this.height / 100 * this.height / 100)
+        this.bmi = bmi.toFixed(2)
       }
     }
-  };
+  }
 </script>
-
-<style scoped>
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-    transform: scale(0.95);
-    /* 改變尺寸 */
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s, transform 0.5s;
-    /* 添加 transform 過渡動畫 */
+  .inner {
+    background: rgb(7, 218, 165);
+    padding: 1em;
+    border-radius: 10px;
+    width: 250px;
+    clip-path: circle(10% at 90% 20%);
+    transition: all .5s ease-in-out;
+    cursor: pointer;
+    position: relative;
   }
 
-  .card {
-    clip-path: polygon(100% 0, 100% 100%, 0 100%);
-    /* 右上角到左下角 */
-    transition: clip-path 0.5s;
-    /* 過渡動畫效果 */
+  .inner:hover {
+    clip-path: circle(75%);
+    background: #00B6FF;
   }
 
-  .card:hover {
-    background-color: rgba(229, 231, 235, 1);
-    /* 背景顏色淺灰色 */
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    /* 陰影效果 */
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-    /* 顯示完整卡片 */
+  .inner span {
+    color: white;
+    font-weight: bold;
+    transition: color .5s;
+    position: absolute;
+    top: 27px;
+    right: 14px;
+    z-index: 2; /* 新增的屬性 */
+  }
+
+  .inner:hover span {
+    color: rgba(255, 255, 255, 0);
+  }
+
+  .inner h1 {
+    color: white;
+    margin: 0;
+  }
+
+  .inner p {
+    color: white;
+    font-size: .8rem;
+  }
+
+  .B-wrap >span{}
+  .content-padding{
+    height:20px;
   }
 </style>
